64043d6105815af73d938a33ba1edce461679c70
Edgar Villalobos <i07villalobos@gmail.com>
2017-08-24 09:26:13 -0700

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,22 @@
+var altura =7;
+var base =5;
+var area = base * altura;
+console.log("Area del rectangulo es igual: "+area);
+//area circulo, triangulo, volumen de esfera
+
+var radio = 2
+var p = Math.PI
+var areacir = p*(radio*2)
+console.log("Area del circulo es igual: "+areacir);
+
+var h = 2
+var b = 4
+var areatri = (b*h)/2
+console.log("Area del triangulo es igual: "+areatri);
+
+var radio3 = 2
+var phi = Math.PI
+var volum = (3/4)*(phi * Math.pow(radio3,3));
+console.log("Volumen de esfera es igual: "+volum);
+
+
