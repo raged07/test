23fca3a299529e72c060a29c1c06daba441a5e78
Edgar Villalobos <i07villalobos@gmail.com>
2017-09-01 19:59:25 -0600

    Javascript30

--- www/js/platzom.js
+++ www/js/platzom.js
@@ -10,24 +10,51 @@ function platzom(str){
     if (str.toLowerCase().startsWith('z')){
         translation +='pe'
     }
+    
+/*Si la palabra traducida tiene 10 o mas letras se debe partir a la mitad y unir con un guion*/
     if(str.length>=10){
-        
         separador = ""
         limite    = str.length/2
         arregloDeSubCadenas = str.split(separador, limite); //A,B,C,D,E...
         arregloAcadena = arregloDeSubCadenas.join("");
-        
         subCadena = str.substring(str.length, str.length/2);//Final de cadena
-        
         translation = `${arregloAcadena}${'-'}${subCadena}`;
-        
+    }
+
+   /* letlength = translation.length
+    if(length >= 10){
+        const fHalf = trnaslation.slice(0, Math.round(length/2));
+        const sHalf = translation.slice(Math.round(length/2));
+        translation = `${fHalf} - ${sHalf}`;
+    }*/
+
+    
+/*Si la palabra original es un palindromo ninguna de las anteriores reglas funciona, y se devuelve la palabra
+intercalando entre mayusculas y minusculas*/
+
+    const reverse = (str)=>+ //es lo mismo que const reverse = function(str)
+    str.split('').reverse().join('');
+    function minMay(str){
+        const l = str.length;
+        let translation = "";
+        let capitalize = true;
+        for (let i=0; i<l;i++){
+            const char = str.charAt(i);
+            translation += capitalize ? char.toUpperCase() : char.toLowerCase();//? comparacion ?
+        }
+        return translation 
+            
+        }
+    }
+    if(str = reverse(str)){
+        return minMay(str);//
     }
   
     return translation
 }
 console.log(platzom("programar"));
 console.log(platzom("Zorro"));
-console.log(platzom("ABCDEFGHIJKMNOPQRSTUVWXYZ"));
+console.log(platzom("ABCDEFGHIJKMNOPQRSTUVWXY"));
 
 
 /*var cadena = "ABCDEFGHIJ";
@@ -39,10 +66,3 @@ arregloAcadena = arregloDeSubCadenas.join("");
 subCadena = cadena.substring(cadena.length, cadena.length/2);//Final de cadena
 
 console.log(arregloAcadena + '-' + subCadena);*/
-
-
-
-
-/*Si la palabra traducida tiene 10 o mas letras se debe partir a la mitad y unir con un guion*/
-/*Si la palabra original es un palindromo ninguna de las anteriores reglas funciona, y se devuelve la palabra
-intercalando entre mayusculas y minusculas*/
